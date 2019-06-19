90b4c19562b0316e0d04c1f9b1b4705fd069f004
Jathur <36088069+JathurL@users.noreply.github.com>
2019-06-19 09:07:03 +0200

    Update j.js

--- j.js
+++ j.js
@@ -5,6 +5,50 @@ var tan = Math.tan
 var arcsin = Math.asin
 var arccos = Math.acos
 var arctan = Math.atan
+var deg = pi/180
+var rad = 180/pi
 var ball = []
+var s = function(x) {
+  return Math.pow(x,2)
+}
+var p = function(x,y) {
+  return Math.pow(x,y)
+}
+var sqrt = function(x) {
+  return Math.sqrt(x)
+}
+var ar = function(x) {
+  return Math.round(x)
+}
+var f = function(x) {
+  return s(x)-x
+}
+var g = function(x) {
+  return (x*3)+x
+}
+ball.radius = function(a) {
+  var result = sqrt(a/(4*pi))
+  return result
+}
+ball.area = function(r) {
+  var result = (s(r)*(4*pi))/3
+  return result
+}
+ball.volume = function(r) {
+  var result = (p(r,3)*(4*pi))/3
+  return result
+}
 
 j = []
+j.calculate = function(input) {
+   var answer = input.toLowerCase();
+   answer = eval(answer)
+   console.log(answer)
+   answer = parseFloat(answer)
+ 
+   if (answer == !undefined || isNaN(answer)) {
+     return 0
+   } else {
+     lastAnswer = answer
+   }
+}
